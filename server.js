873a84a0ee0e03c9da3b96451f51a516166cb78a
mummick <mummick6@gmail.com>
2021-06-27 18:48:37 +0300

    feat: add TypeScript support

--- server.js
+++ server.js
@@ -8,12 +8,13 @@ const testService = require('./services/testService');
 const todoListService = require('./services/todoList/todoListService');
 const authService = require('./authService');
 
-const {SocketServer} = require('./socketService');
+const { SocketServer } = require('./socketService');
 
 const app_port = process.env.app_port || 4040;
 const app_host = process.env.app_host || '127.0.0.1';
 
 const base = require('./dbService');
+import { CellCoord } from './chess/chess-lib/cell-coord';
 
 function paramsParser(paramsString) {
   let params = {};
@@ -25,16 +26,14 @@ function paramsParser(paramsString) {
 }
 
 class Server {
-  constructor() {
-
-  }
+  constructor() {}
 
   response(res, val) {
     res.writeHead(200, {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
-      'Access-Control-Allow-Headers': 'X-PINGOTHER, Content-Type'
+      'Access-Control-Allow-Headers': 'X-PINGOTHER, Content-Type',
     });
 
     res.write(val);
@@ -42,23 +41,25 @@ class Server {
   }
 
   start() {
-    http.createServer(async (req, res) => {
-      let entry = req.url.split('?');
-      let route = entry[0].slice(1);
-      let params = paramsParser(entry[1] || '');
-      try {
-        let userData = null;
-        console.log(params);
-        if (params.sessionId){
-          userData = await authService.getUserBySessionId(params.sessionId);
-          console.log(userData);
+    http
+      .createServer(async (req, res) => {
+        let entry = req.url.split('?');
+        let route = entry[0].slice(1);
+        let params = paramsParser(entry[1] || '');
+        try {
+          let userData = null;
+          console.log(params);
+          if (params.sessionId) {
+            userData = await authService.getUserBySessionId(params.sessionId);
+            console.log(userData);
+          }
+          let result = await router.route(route, params, userData);
+          this.response(res, JSON.stringify(result));
+        } catch (err) {
+          this.response(res, JSON.stringify(err));
         }
-        let result = await router.route(route, params, userData);
-        this.response(res, JSON.stringify(result));
-      } catch (err) {
-        this.response(res, JSON.stringify(err));
-      }
-    }).listen(app_port);
+      })
+      .listen(app_port);
   }
 }
 
@@ -71,6 +72,10 @@ todoListService.start();
 authService.start();
 
 new SocketServer();
+
+// CellCoord описана в TS файле. Ниже 2 строчки подтверждают, что TS компилируется и нормально интегрируется в JS проект.
+const coords = new CellCoord(1, 3);
+console.log(coords.toString());
 
 console.log('Web server running at http://' + app_host + ':' + app_port);
 
